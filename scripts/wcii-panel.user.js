13829ab4ec201a99a2c9d1f6440eca64f94f760e
Roman A <x.wserfer@gmail.com>
2020-02-02 20:21:55 +0300

    Create wcii-panel.user.js

---
+++ scripts/wcii-panel.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @namespace          https://wcii.ru/wcii-panel
+// @name               WCII Panel
+// @version            0.0.1
+// @author             [webserfer]
+// @website            https://wcii.ru
+// @update             https://github.com/webserfer/wcii-scripts/raw/master/scripts/wcii-panel.user.js
+// @icon               https://raw.githubusercontent.com/webserfer/wcii-scripts/master/icons/wcii-panel.png
+// @match              https://*
+// @grant              none
+// ==/UserScript==
